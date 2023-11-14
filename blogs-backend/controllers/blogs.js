03e0abfeb91eee0479cf7c802a2c0be8ebad4aba
Rizvan <rizvan.chalilovas@proton.me>
2023-11-15 00:48:29 +0200

    ex.: 13.7

--- blogs-backend/controllers/blogs.js
+++ blogs-backend/controllers/blogs.js
@@ -12,38 +12,26 @@ router.get('/', async (req, res) => {
 })
 
 router.post('/', async (req, res) => {
-  try {
-    const blog = await Blog.create(req.body)
-    res.json(blog)
-  } catch (error) {
-    res.status(400).json({error })
-  }
+  const blog = await Blog.create(req.body)
+  res.json(blog)
 })
 
 router.put('/:id', blogMiddleware, async (req, res) => {
-  try {
-    if (req.blog) {
-      req.blog.likes = req.body.likes
-      await req.blog.save()
-      res.json({ likes: req.blog.likes })
-    } else {
-      res.status(404).end()
-    }
-  } catch (error) {
-    res.status(400).json({ error })
+  if (req.blog) {
+    req.blog.likes = req.body.likes
+    await req.blog.save()
+    res.json({ likes: req.blog.likes })
+  } else {
+    res.status(404).end()
   }
 })
 
 router.delete('/:id', blogMiddleware, async (req, res) => {
-  try {
-    if (req.blog) {
-      await req.blog.destroy()
-      res.status(204).end()
-    } else {
-      res.status(404).end()
-    }
-  } catch (error) {
-    res.status(400).json({ error })
+  if (req.blog) {
+    await req.blog.destroy()
+    res.status(204).end()
+  } else {
+    res.status(404).end()
   }
 })
 
--- blogs-backend/index.js
+++ blogs-backend/index.js
@@ -1,11 +1,27 @@
-const express = require('express')
-const { connectToDatabase } = require('./util/db')
 const { PORT } = require('./util/config.js');
+const express = require('express')
+require('express-async-errors')
+const { connectToDatabase } = require('./util/db')
 const blogsRouter = require('./controllers/blogs')
 const app = express()
 
+const errorHandler = (error, request, response, next) => {
+  console.log('errorHandler:', error.name);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ errors: [ 'malformatted id' ] })
+  }
+
+  if (error.name === 'SequelizeValidationError') {
+    return response.status(400).send({ errors: error.errors.map(err => err.message) })
+  }
+
+  next(error)
+}
+
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+app.use(errorHandler)
 
 const start = async () => {
   await connectToDatabase();
