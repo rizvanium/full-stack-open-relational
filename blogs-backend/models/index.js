db696f385537ddabda8cd65d10f44798cc576a07
Rizvan <rizvan.chalilovas@proton.me>
2023-11-22 22:38:37 +0200

    ex.: 13.19

--- blogs-backend/models/index.js
+++ blogs-backend/models/index.js
@@ -1,10 +1,15 @@
 const Blog = require('./blog.js')
 const User = require('./user.js')
+const ReadingList = require('./readingList.js')
 
 User.hasMany(Blog)
 Blog.belongsTo(User)
 
+User.belongsToMany(Blog, { through: ReadingList })
+Blog.belongsToMany(User, { through: ReadingList })
+
 module.exports = {
   Blog,
   User,
+  ReadingList,
 }
